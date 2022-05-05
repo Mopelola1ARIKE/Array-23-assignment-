3940a243afc71b90b7628b64d180ec084ed17e08
Mopelola1ARIKE <98586013+Mopelola1ARIKE@users.noreply.github.com>
2022-05-05 11:13:38 +0100

    array

---
+++ app.js
@@ -0,0 +1,24 @@
+// Qestion1
+
+let array1 = [20, 46, 90, 80, 120];
+
+let array2 = [2, 2, 5, 4, 2]; 
+
+var set1 =array1[0] / array2[0];
+var set2 =array1[1] / array2[1];
+var set3 =array1[2] / array2[2];
+var set4 =array1[3] / array2[3];
+var set5 =array1[4] / array2[4];
+
+
+alert(set1);
+alert(set2);
+alert(set3);
+alert(set4);
+alert(set5);
+
+let array3 = new Array();
+
+array3.push(set1,set2,set3,set4,set5);
+
+alert(array3);
